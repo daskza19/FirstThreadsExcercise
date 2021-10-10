2f1f795acd30957962b32c34deede0f5aa040925
daskza19 <josep.sanchez.arbona@gmail.com>
2021-10-10 19:38:17 +0200

    Some Improvements

--- Lab Session 2/Lab Session 2/Assets/Scripts/ClientUDP.cs
+++ Lab Session 2/Lab Session 2/Assets/Scripts/ClientUDP.cs
@@ -1,25 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
 
 using System.Net;
 using System.Net.Sockets;
 
 public class ClientUDP : MonoBehaviour
 {
-    //public Socket newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-    //public IPEndPoint ipep = new IPEndPoint("192.168.1.45", 3000);
-    //public EndPoint sendEnp;
+    //UDP Things
+    private Socket newSocket;
+    private IPEndPoint ipep;
+    private EndPoint sendEnp;
+
+    private Thread mainThread;
+
+    //Properties
+    public string message = "PING";
+    public int loops = 5;
+    private int actualloops;
+    public int delayTime = 5000;
+    private bool isEnded;
+    public Text uiText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isEnded = false;
+        actualloops = 0;
+
+        newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        ipep = new IPEndPoint((int)float.Parse("192.168.1.45"), 7000);
+        sendEnp = (EndPoint)ipep;
+
+        mainThread = new Thread(MainLoop);
+        mainThread.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MainLoop()
     {
-        
+        while (isEnded == false)
+        {
+            byte[] buffer = new byte[256];
+            newSocket.SendTo(System.Convert.FromBase64String(message), System.Convert.FromBase64String(message).Length, SocketFlags.None, sendEnp);
+            Debug.Log("Sended: " + message);
+            //ChangeUIText(message);
+
+            int recv = newSocket.ReceiveFrom(buffer, ref sendEnp); //Receive from a client and do the debug
+            Debug.Log("Received: " + System.Convert.ToBase64String(buffer));
+            //ChangeUIText(System.Convert.ToBase64String(buffer));
+
+            Thread.Sleep(delayTime); //Do a delay (like the statement says)
+
+            //Actualize the counter and the bool to exit the while
+            actualloops++;
+            if (actualloops >= loops)
+                isEnded = true;
+        }
+    }
+
+    private void ChangeUIText(string _text)
+    {
+        uiText.text = _text;
     }
 }
--- Lab Session 2/Lab Session 2/Assets/Scripts/ServerUDP.cs
+++ Lab Session 2/Lab Session 2/Assets/Scripts/ServerUDP.cs
@@ -1,32 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TextCore;
+using System.Threading;
 
 using System.Net;
 using System.Net.Sockets;
 
 public class ServerUDP : MonoBehaviour
 {
-    public Socket newSocket;
-    public IPEndPoint ipep;
-    public EndPoint sendEnp;
+    //UDP Things
+    private Socket newSocket;
+    private IPEndPoint ipep;
+    private EndPoint sendEnp;
+
+    private Thread mainThread;
+
+    //Properties
+    public string message = "PONG";
+    public int loops = 5;
+    private int actualloops;
+    public int delayTime = 5000;
+    private bool isEnded;
 
     // Start is called before the first frame update
     void Start()
     {
+        isEnded = false;
+        actualloops = 0;
         newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         ipep = new IPEndPoint(IPAddress.Any, 7000);
         sendEnp = (EndPoint)ipep;
 
         newSocket.Bind(ipep);
+        mainThread = new Thread(MainLoop);
+        mainThread.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MainLoop()
     {
-        
+        while (isEnded == false)
+        {
+            byte[] buffer = new byte[256];
+            int recv = newSocket.ReceiveFrom(buffer, ref sendEnp); //Receive from a client and do the debug
+            Debug.Log("Received: " + System.Convert.ToBase64String(buffer));
+
+            Thread.Sleep(delayTime); //Do a delay (like the statement says)
+
+            newSocket.SendTo(System.Convert.FromBase64String(message), recv, SocketFlags.None, sendEnp);
+
+            //Actualize the counter and the bool to exit the while
+            //actualloops++;
+            //if (actualloops>= loops)
+            //    isEnded = true;
+        }
     }
 
-
+    private void OnDestroy()
+    {
+        newSocket.Close();
+    }
 }
